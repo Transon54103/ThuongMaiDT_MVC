a6fa3f9629504af4515c665ccce5100b2252248b
sonng <sonng@VANSON>
2024-05-28 00:27:29 +0700

    Add project files.

---
+++ Project_ThuongMaiDT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Project_ThuongMaiDT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
