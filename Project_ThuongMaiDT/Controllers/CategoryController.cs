010008894b474ab335cbd286756405b25c3499b4
vanson <sonnghean54@gmail.com>
2024-05-31 02:37:46 +0700

    #

---
+++ Project_ThuongMaiDT/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project_ThuongMaiDT.Data;
+using Project_ThuongMaiDT.Models;
+
+namespace Project_ThuongMaiDT.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _dB;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _dB = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objcategorylist = _dB.categories.ToList();
+            return View(objcategorylist);
+        }
+        public IActionResult Create() 
+        {
+            return View();
+        }
+    }
+}
---
+++ Project_ThuongMaiDT/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project_ThuongMaiDT.Models;
+
+namespace Project_ThuongMaiDT.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+        public DbSet<Category> categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1},
+                new Category { Id = 2, Name = "History", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "SciFi", DisplayOrder = 3 }
+                );
+        }
+    }
+}
